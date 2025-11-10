b2f88ab95859052136eec9352d2e8a487810d4a1
tsandeepkumar002-byte <tsandeepkumar002@gmail.com>
2025-11-10 20:25:49 +0530

    Delete api.js

--- api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const API_BASE = 'https://epaper-meesakshinews-g19d.onrender.com'
-
-export const api = axios.create({
-  baseURL: API_BASE,
-  headers: { 'Content-Type': 'application/json' }
-})
