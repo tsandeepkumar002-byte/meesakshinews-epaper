39192aaa5bff332df5b98b7f05b18410bf3f1393
tsandeepkumar002-byte <tsandeepkumar002@gmail.com>
2025-11-10 20:54:27 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,8 +1,31 @@
-import something from "./api";
+// src/api.js
 
-export const API_BASE = 'https://epaper-meesakshinews-g19d.onrender.com'
+import axios from "axios";
 
+export const BASE_URL = "https://epaper-meesakshinews-g19d.onrender.com";
+
+// Create a reusable axios instance
 export const api = axios.create({
-  baseURL: API_BASE,
-  headers: { 'Content-Type': 'application/json' }
-})
+  baseURL: BASE_URL,
+});
+
+// Example API functions
+export async function loginUser(email, password) {
+  try {
+    const res = await api.post("/login", { email, password });
+    return res.data;
+  } catch (err) {
+    console.error("Login error:", err);
+    throw err;
+  }
+}
+
+export async function getEditions() {
+  try {
+    const res = await api.get("/editions");
+    return res.data;
+  } catch (err) {
+    console.error("Get editions error:", err);
+    throw err;
+  }
+}
